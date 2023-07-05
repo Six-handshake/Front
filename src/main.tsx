32eff0f0d857536ba673456d9f6767d18ac7a637
AnnaErs <annaershova02@gamil.com>
2023-07-05 14:59:39 +0500

    fix: new Todos

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ChakraProvider } from "@chakra-ui/react";
+import {
+    Box,
+    Button,
+    Flex,
+    Input,
+    InputGroup,
+    Modal,
+    ModalBody,
+    ModalCloseButton,
+    ModalContent,
+    ModalFooter,
+    ModalHeader,
+    ModalOverlay,
+    Stack,
+    Text,
+    useDisclosure,
+} from "@chakra-ui/react";
 
 import { App } from "./app";
+import { Todos } from "./todos";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
         <App />
+        <Todos />
     </React.StrictMode>
 );
