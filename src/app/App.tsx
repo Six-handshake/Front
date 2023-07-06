bcac4321ca504b9ec67ac41568e1d6a7981a9667
AnnaErs <annaershova02@gamil.com>
2023-07-06 13:25:25 +0500

    little fix todos

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,35 @@
 import { CardChild } from "../shared/cards";
 import { InputKonterAgent } from "../shared/input";
 import { Container } from "../shared/container";
-import { Todos } from "../todos";
+import React, { useEffect, useState } from "react";
+import { Stack } from "@chakra-ui/react";
 
 function App() {
+    const TodosContext = React.createContext({
+        todos: [],
+        fetchTodos: () => {},
+    });
+
+    function Todos() {
+        const [todos, setTodos] = useState([]);
+        const fetchTodos = async () => {
+            const response = await fetch("http://localhost:1234/todo");
+            const todos = await response.json();
+            setTodos(todos.data);
+        };
+        useEffect(() => {
+            fetchTodos();
+        }, []);
+        return (
+            <TodosContext.Provider value={{ todos, fetchTodos }}>
+                <Stack spacing={5}>
+                    {todos.map((todo) => (
+                        <b>{todo.message}</b>
+                    ))}
+                </Stack>
+            </TodosContext.Provider>
+        );
+    }
     return (
         <Container>
             <div className="w-full h-screen flex flex-col justify-left gap-7">
@@ -23,9 +49,7 @@ function App() {
                         id="2"
                     />
                 </div>
-                <div>
-                    <Todos/>
-                </div>
+                <div>{Todos()}</div>
             </div>
         </Container>
     );
