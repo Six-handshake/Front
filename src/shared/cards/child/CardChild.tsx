1b67811b29324dc3cdeb49610399c87b177527b8
AnnaErs <annaershova02@gamil.com>
2023-07-06 13:03:43 +0500

    feat: ToSH-5 [FRONTEND] Компонент импута

--- src/shared/cards/child/CardChild.tsx
+++ src/shared/cards/child/CardChild.tsx
@@ -1,16 +1,30 @@
 import { memo } from "react";
 import { Card, Typography } from "antd";
 import { CompassOutlined, PhoneOutlined } from "@ant-design/icons";
-import { CardChildProps } from "./types";
+import { CardChildPropsType } from "./types";
 
-const CardChild = memo<CardChildProps>(
-    function CardChild({companyName, description, adress}) {
-        return( 
-        <Card title={companyName} extra={<a href="#">More</a>} style={{ width: 300 }} className='m-4'>
+const CardChild = memo<CardChildPropsType>(function CardChild({
+    companyName,
+    description,
+    adress,
+    phone,
+}) {
+    return (
+        <Card
+            title={companyName}
+            extra={<a href="#">More</a>}
+            style={{ width: 300 }}
+            className=""
+        >
             <Typography.Paragraph>{description}</Typography.Paragraph>
-            <Typography.Paragraph><CompassOutlined /> Адресс {adress}</Typography.Paragraph>
-            <Typography.Paragraph><PhoneOutlined /> Телефон</Typography.Paragraph>
-        </Card>);
+            <Typography.Paragraph>
+                <CompassOutlined /> Адресс {adress}
+            </Typography.Paragraph>
+            <Typography.Paragraph>
+                <PhoneOutlined /> Телефон {phone}
+            </Typography.Paragraph>
+        </Card>
+    );
 });
 
 export { CardChild };
---
+++ src/shared/input/types.ts
@@ -0,0 +1,6 @@
+export type InputKonterAgentPropsType = {}
+
+export type MyFormItemGroupPropsType =  {
+    prefix: string | number | (string | number)[];
+    children: React.ReactNode;
+  }
